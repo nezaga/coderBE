2cdb1c43faf650203a9328f7794d34064961c99e
nezaga <nicolas.zaga@gmail.com>
2023-04-25 11:11:19 -0300

    preparando proyecto para motor de plantillas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
-const express = require('express')
-const products_router = require('./routes/products_routes');
-const carts_router = require('./routes/carts_routes');
+import express, { json, urlencoded } from 'express';
+import products_router from './routes/products_routes';
+import carts_router from './routes/carts_routes';
+import handlebars from 'express-handlebars';
+import __dirname from './utils';
 
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 app.use('/api', products_router);
 app.use('/api', carts_router);
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname+'(/views')
+app.set('view engine', 'handlebars')
+
 
 app.listen(8080, () => {
     console.log('Executing in Port 8080');
